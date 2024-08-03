42291004d22e590a120a75aa662607314871d990
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2024-08-03 16:42:40 +0000

    commit Education.jsx

---
+++ src/Components/Education.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="light" id="education">
+      <h2 style={{ color: "black" }}>Education</h2>
+      <div className="education">
+        <div>
+          <h3>Indian Institute Of Technology Madras</h3>
+          <p>Bachelors of Science in Data Science and Programing</p>
+          <p>2024 - 2028</p>
+        </div>
+        <div>
+          <h3>Keshav Memotial Institute Of Technology</h3>
+          <p>Bachelor of Technology in Computer Science and Engineering</p>
+          <p>2024 - 2028</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
