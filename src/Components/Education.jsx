8a106483502783eb95f51fe94addbcb1119d6409
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2024-08-06 23:25:36 +0530

    Update Education.jsx

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -10,11 +10,11 @@ const Education = () => {
           <p>Bachelors of Science in Data Science and Programing</p>
           <p>2024 - 2028</p>
         </div>
-        <div>
+{/*         <div>
           <h3>Keshav Memotial Institute Of Technology</h3>
           <p>Bachelor of Technology in Computer Science and Engineering</p>
           <p>2024 - 2028</p>
-        </div>
+        </div> */}
       </div>
     </section>
   );
