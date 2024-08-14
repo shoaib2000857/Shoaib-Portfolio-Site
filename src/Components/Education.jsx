767134b56663230e56ede57e3d02c0580afe4f63
SHOAIB SADIQ SALEHMOHAMED <24f2000857@ds.study.iitm.ac.in>
2024-08-14 15:39:29 +0000

    .

--- src/Components/Education.jsx
+++ src/Components/Education.jsx
@@ -5,13 +5,15 @@ const Education = () => {
     <section className="light" id="education">
       <h2 style={{ color: "black" }}>Education</h2>
       <div className="education">
-        <div style ={{flexWrap: "wrap"}}>
+        <div style={{ flexWrap: "wrap" }}>
           <h3>Indian Institute Of Technology Madras</h3>
           <p>Bachelors of Science in Data Science and Programing</p>
           <p>2024 - 2028</p>
         </div>
-        <div style ={{flexWrap: "wrap", marginTop: "3em"}}>
-          <h3><a href="/certifications">Certifications</a></h3>
+        <div style={{ flexWrap: "wrap", marginTop: "3em" }}>
+          <h3>
+            <a href="/certifications">Certifications</a>
+          </h3>
         </div>
       </div>
     </section>
